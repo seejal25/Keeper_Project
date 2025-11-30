cc28a40609987e3aac1f160d8a5ebcf0bb471f52
seejal25 <seejalsiva@gmail.com>
2025-11-30 13:45:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,39 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import CreateArea from "./CreateArea";
+import Note from "./Note";
+import "./index.css";
 
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prev => [...prev, newNote]);
+  }
+
+  function deleteNote(id) {
+    setNotes(prev =>
+      prev.filter((note, index) => index !== id)
+    );
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          color={note.color}
+          onDelete={deleteNote}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
